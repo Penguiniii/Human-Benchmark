848f7e5a051af938a4f1277df17adff555560430
Penguiniii <63211745+Penguiniii@users.noreply.github.com>
2020-04-06 21:43:22 +0100

    Getting the data from the database

--- pages/home.php
+++ pages/home.php
@@ -7,5 +7,24 @@
 		<td>User</td>
 		<td>Score</td>
 	</tr>
+
+
+	<?php
+	
+	$result = mysql_query("SELECT use, score FROM leaderboard ORDER BY score DESC");
+	$rank = 1;
+
+		if(mysql_num_rows($relust)) {
+			while($row = mysql_fetch_assoc($result)) {
+				echo "<td>{$rank}</td>
+					  <td>{$row['user']}</td>
+					  <td>{$row['score']}</td>"
+
+				$rank++;
+			}
+		}
+
+?>
+
 </table>
 
